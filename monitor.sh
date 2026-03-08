a3f5a9aa41eb27c8c86f56e5d457d0cbbc9d2b4b
Anuj-pawar-dev <anujpawar229@gmail.com>
2026-03-09 00:14:45 +0530

    Added Linux system monitoring script

--- monitor.sh
+++ monitor.sh
@@ -20,3 +20,10 @@ echo""
 echo "top 5 processes by memory usage:"
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 6
 
+cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+cpu_int=${cpu%.*}
+if [ "$cpu_int" -gt 80]
+then
+	echo"WARNING: High CPU Usage!"
+fi
+
